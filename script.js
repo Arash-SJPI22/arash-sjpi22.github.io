fb5cc4dd28716e4e0ac049222268589de839c318
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2022-10-04 21:50:09 +0200

    Added links, descripton and github repo links and function for creating dl,dt and dd from them and genereate to display

--- script.js
+++ script.js
@@ -1,74 +1,109 @@
 const date = new Date();
+// Counters to add a function that creates const = document.createElement("dl/li") to the number of (courses?) ownProjects and assignments in each course.
+let ownProjectCount = 0, assignmentCount = 0;
 
-const kurser = [
+const courses = [
     {
-        kursNamn: "Introduktion till Webbutveckling",
-        kursStart: new Date("2022-09-05"),
-        kursSlut: new Date("2022-10-09"),
+        courseName: "Introduktion till Webbutveckling",
+        courseStart: new Date("2022-09-05"),
+        courseEnd: new Date("2022-10-09"),
+        ownProjects: [
+        {   
+            descripton: "En lite frågesportssida - HTML/CSS & JS",
+            link: "https://arash-sjpi22.github.io/gissning/",
+            linkDescription: "Gissning",
+            link2: "https://github.com/arash-sjpi22/gissning",
+        },
+        {
+            descripton: "Denna sida - Genererad från en Array - HTML/CSS & JS",
+            link: "https://arash-sjpi22.github.io/",
+            linkDescription: "Projekt sida",
+            link2: "https://github.com/Arash-SJPI22/arash-sjpi22.github.io",
+        },],
+        assignment:[
+        {
+            descripton: "Första kodningsuppgiften - HTML/CSS & JS",
+            link: "https://arash-sjpi22.github.io/webutv.upp3/",
+            linkDescription: "Inlämningsuppgift 3",
+            link2: "https://github.com/arash-sjpi22/webutv.upp3/",
+        },
+        {
+            descripton: "En ToDo-lista med minial styling - HTML/CSS & JS",
+            link: "https://arash-sjpi22.github.io/todo/",
+            linkDescription: "Inlämningsuppgift 4",
+            link2: "https://github.com/Arash-SJPI22/todo",
+        },
+        {
+            descripton: "En digital CV - HTML/CSS & JS",
+            link: "https://arash-sjpi22.github.io/arash-sjpi22/",
+            linkDescription: "Inlämningsuppgift 7",
+            link: "https://github.com/Arash-SJPI22/arash-sjpi22",
+        },], 
     },
     {
-        kursNamn: "Webbutveckling - HTML & CSS",
-        kursStart: new Date("2022-10-10"),
-        kursSlut: new Date("2022-11-06"),
+        courseName: "Webbutveckling - HTML & CSS",
+        courseStart: new Date("2022-10-10"),
+        courseEnd: new Date("2022-11-06"),
     },
     {
-        kursNamn: "Webbutveckling - JavaScript",
-        kursStart: new Date("2022-11-07"),
-        kursSlut: new Date("2022-12-04"),
+        courseName: "Webbutveckling - JavaScript",
+        courseStart: new Date("2022-11-07"),
+        courseEnd: new Date("2022-12-04"),
     },
     {
-        kursNamn: "Arbetsmetodik och Agila projektmetoder",
-        kursStart: new Date("2022-12-05"),
-        kursSlut: new Date("2023-01-08"),
+        courseName: "Arbetsmetodik och Agila projektmetoder",
+        courseStart: new Date("2022-12-05"),
+        courseEnd: new Date("2023-01-08"),
     },
     {
-        kursNamn: "Avancerad webbutveckling - JavaScript Backend",
-        kursStart: new Date("2023-01-09"),
-        kursSlut: new Date("2023-02-12"),
+        courseName: "Avancerad webbutveckling - JavaScript Backend",
+        courseStart: new Date("2023-01-09"),
+        courseEnd: new Date("2023-02-12"),
     },
     {
-        kursNamn: "Interaktionsdesign och användbarhet",
-        kursStart: new Date("2023-02-13"),
-        kursSlut: new Date("2023-03-12"),
+        courseName: "Interaktionsdesign och användbarhet",
+        courseStart: new Date("2023-02-13"),
+        courseEnd: new Date("2023-03-12"),
     },
     {
-        kursNamn: "Avancerad webbutveckling - JavaScript Full Stack",
-        kursStart: new Date("2023-03-13"),
-        kursSlut: new Date("2023-05-21"),
+        courseName: "Avancerad webbutveckling - JavaScript Full Stack",
+        courseStart: new Date("2023-03-13"),
+        courseEnd: new Date("2023-05-21"),
     },
     {
-        kursNamn: "Systemutveckling Java",
-        kursStart: new Date("2023-08-21"),
-        kursSlut: new Date("2023-09-17"),
+        courseName: "Systemutveckling Java",
+        courseStart: new Date("2023-08-21"),
+        courseEnd: new Date("2023-09-17"),
     },
     {
-        kursNamn: "Systemarkitektur",
-        kursStart: new Date("2023-09-18"),
-        kursSlut: new Date("2023-10-22"),
+        courseName: "Systemarkitektur",
+        courseStart: new Date("2023-09-18"),
+        courseEnd: new Date("2023-10-22"),
     },
     {
-        kursNamn: "Backendutveckling och systemintegration",
-        kursStart: new Date("2023-10-23"),
-        kursSlut: new Date("2023-12-31"),
+        courseName: "Backendutveckling och systemintegration",
+        courseStart: new Date("2023-10-23"),
+        courseEnd: new Date("2023-12-31"),
     },
     {
-        kursNamn: "LIA",
-        kursStart: new Date("2024-02-05"),
-        kursSlut: new Date("2024-05-26"),
+        courseName: "LIA",
+        courseStart: new Date("2024-02-05"),
+        courseEnd: new Date("2024-05-26"),
     },
     {
-        kursNamn: "Examensarbete",
-        kursStart: new Date("2023-05-22"),
-        kursSlut: new Date("2024-06-06"),
+        courseName: "Examensarbete",
+        courseStart: new Date("2023-05-22"),
+        courseEnd: new Date("2024-06-06"),
     },
 ];
 
 for (let i=0; i < 12; i++) {
-    const courses = document.querySelector("#courses");
+    const coursesUl = document.querySelector("#coursesUl");
     const intro = document.querySelector("#intro");
 
     const aSS = document.createElement("span");
-    const aS = document.createElement("a");
+    const aS1 = document.createElement("a");
+    const aS2 = document.createElement("a");
     const liS1 = document.createElement("li");
     const liS2 = document.createElement("li");
     const h3S = document.createElement("h3");
@@ -76,13 +111,15 @@ for (let i=0; i < 12; i++) {
     const h4SI = document.createElement("h4");
     const dlS1 = document.createElement("dl");
     const dlS2 = document.createElement("dl");
+    const ghRepo = "GitHub repo";
+
     let j = i + 1;
 
     intro.appendChild(liS1);
-    liS1.appendChild(aS);
+    liS1.appendChild(aS1);
     liS1.appendChild(aSS);
     
-    courses.appendChild(liS2);
+    coursesUl.appendChild(liS2);
     liS2.appendChild(h3S);
     liS2.appendChild(h4SP);
     liS2.appendChild(dlS1);
@@ -90,30 +127,81 @@ for (let i=0; i < 12; i++) {
     liS2.appendChild(dlS2);
 
     if (i + 1 < 10) {
-        aS.setAttribute("href", "#kurs0" + j);
-        aS.innerText = "0" + j + " " + kurser[i].kursNamn;
+        aS1.setAttribute("href", "#kurs0" + j);
+        aS1.innerText = "0" + j + " " + courses[i].courseName;
 
         h3S.setAttribute("id", "kurs0" + j);    
-        h3S.innerText = "0" + j + " " + kurser[i].kursNamn;
+        h3S.innerText = "0" + j + " " + courses[i].courseName;
     } else {
-        aS.setAttribute("href", "#kurs" + j);
-        aS.innerText = j + " " + kurser[i].kursNamn;
+        aS1.setAttribute("href", "#kurs" + j);
+        aS1.innerText = j + " " + courses[i].courseName;
 
         h3S.id = "kurs" + j;    
-        h3S.innerText = j + " " + kurser[i].kursNamn;
+        h3S.innerText = j + " " + courses[i].courseName;
     };
 
     h4SP.innerText = "Egna Projekt";
     h4SI.innerText = "Inlämningsuppgifter";
 
-    if ((date >= kurser[i].kursStart) && (date < kurser[i].kursSlut)) {
+    if ((date >= courses[i].courseStart) && (date < courses[i].courseEnd)) {
         aSS.innerText += " Kursen Pågår";
         liS1.setAttribute("class", "courseOnGoing");
-    } else if ((date > kurser[i].kursStart) && (date > kurser[i].kursSlut)) {
+    } else if ((date > courses[i].courseStart) && (date > courses[i].courseEnd)) {
         aSS.innerText += " Avslutad Kurs";
         liS1.setAttribute("class", "courseFinished");
-    } else if (date < kurser[i].kursStart) {
+    } else if (date < courses[i].courseStart) {
         aSS.innerText += " Kursen Ej Påbörjad";
         liS1.setAttribute("class", "courseNotStarted");
+    };
+    
+    if ('ownProjects' in courses[i]) {
+        for (let y=0; y < courses[i].ownProjects.length; y++){
+            const ddS1 = document.createElement("dd");
+            const ddS2 = document.createElement("dd");
+            const dtS = document.createElement("dt");
+            const a_S1 = document.createElement("a");
+            const a_S2 = document.createElement("a");
+
+            dlS1.appendChild(dtS);
+            dtS.innerText = courses[i].ownProjects[y].descripton;
+
+            dlS1.appendChild(ddS1);
+            ddS1.appendChild(a_S1);
+            a_S1.setAttribute("href", courses[i].ownProjects[y].link);
+            a_S1.setAttribute("target", "_blank");
+            a_S1.innerText = courses[i].ownProjects[y].linkDescription;
+
+            dlS1.appendChild(ddS2);
+            ddS2.appendChild(a_S2);
+            a_S2.setAttribute("href", courses[i].ownProjects[y].link2);
+            a_S2.setAttribute("target", "_blank");
+            a_S2.innerText = ghRepo;
+
+        };
+    }; 
+    if ('assignment' in courses[i]) {
+        for (let y=0; y < courses[i].assignment.length; y++){
+            const ddS1 = document.createElement("dd");
+            const ddS2 = document.createElement("dd");
+            const dtS = document.createElement("dt");
+            const a_S1 = document.createElement("a");
+            const a_S2 = document.createElement("a");
+
+            dlS2.appendChild(dtS);
+            dtS.innerText = courses[i].assignment[y].descripton;
+
+            dlS2.appendChild(ddS1);
+            ddS1.appendChild(a_S1);
+            a_S1.setAttribute("href", courses[i].assignment[y].link);
+            a_S1.setAttribute("target", "_blank");
+            a_S1.innerText = courses[i].assignment[y].linkDescription;
+
+            dlS2.appendChild(ddS2);
+            ddS2.appendChild(a_S2);
+            a_S2.setAttribute("href", courses[i].assignment[y].link2);
+            a_S2.setAttribute("target", "_blank");
+            a_S2.innerText = ghRepo;
+
+        };
     };
 };
